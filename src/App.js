b23004ddf65c291f30d9b621438d54326ec2c80f
James Sheehan <jmsdevx@gmail.com>
2019-01-29 11:50:17 -0600

    basic layout and theme switch logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./styles/App.css";
+import Main from "./components/layout/Main";
+import { MuiThemeProvider } from "@material-ui/core";
+import {
+  Caregiver,
+  Creator,
+  Explorer,
+  Hero,
+  Innocent,
+  Jester,
+  Lover,
+  Magician,
+  Everyperson,
+  Revolutionary,
+  Ruler,
+  Sage
+} from "./styles/themes";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      theme: "Everyperson"
+    };
+  }
+
+  switchTheme = theme => this.setState({ theme: theme });
+
   render() {
+    let themeSwitch = (function(theme) {
+      switch (theme) {
+        case "Caregiver":
+          return Caregiver;
+        case "Creator":
+          return Creator;
+        case "Explorer":
+          return Explorer;
+        case "Hero":
+          return Hero;
+        case "Innocent":
+          return Innocent;
+        case "Jester":
+          return Jester;
+        case "Lover":
+          return Lover;
+        case "Magician":
+          return Magician;
+        case "Everyperson":
+          return Everyperson;
+        case "Revolutionary":
+          return Revolutionary;
+        case "Ruler":
+          return Ruler;
+        case "Sage":
+          return Sage;
+        default:
+          return Hero;
+      }
+    })(this.state.theme);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider theme={themeSwitch}>
+        <div>
+          <Main theme={this.state.theme} switchTheme={this.switchTheme} />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/styles/themes.js
@@ -0,0 +1,121 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const Caregiver = createMuiTheme({
+  palette: {
+    primary: {
+      dark: "#a84632",
+      main: "#f16548",
+      light: "#f3836c"
+    }
+  }
+});
+
+export const Creator = createMuiTheme({
+  palette: {
+    primary: {
+      dark: "#ac6613",
+      main: "#f7921c",
+      light: "#f8a749"
+    }
+  }
+});
+
+export const Explorer = createMuiTheme({
+  palette: {
+    primary: {
+      dark: "#5a5b20",
+      main: "#81822e",
+      light: "#9a9b57"
+    }
+  }
+});
+
+export const Hero = createMuiTheme({
+  palette: {
+    primary: {
+      dark: "#004877",
+      main: "#0067ab",
+      light: "#3385bb"
+    }
+  }
+});
+
+export const Innocent = createMuiTheme({
+  palette: {
+    primary: {
+      dark: "#007a9a",
+      main: "#00afdd",
+      light: "#33bfe3"
+    }
+  }
+});
+
+export const Jester = createMuiTheme({
+  palette: {
+    primary: {
+      dark: "#738f27",
+      main: "#a5cd39",
+      light: "#b7d760"
+    }
+  }
+});
+
+export const Lover = createMuiTheme({
+  palette: {
+    primary: {
+      dark: "#971f48",
+      main: "#d92d68",
+      light: "#e05786"
+    }
+  }
+});
+
+export const Magician = createMuiTheme({
+  palette: {
+    primary: {
+      dark: "#651a63",
+      main: "#91268e",
+      light: "#a751a4"
+    }
+  }
+});
+
+export const Everyperson = createMuiTheme({
+  palette: {
+    primary: {
+      dark: "#006369",
+      main: "#008e97",
+      light: "#33a4ab"
+    }
+  }
+});
+
+export const Revolutionary = createMuiTheme({
+  palette: {
+    primary: {
+      dark: "#8f1b27",
+      main: "#cd2738",
+      light: "#d7525f"
+    }
+  }
+});
+
+export const Ruler = createMuiTheme({
+  palette: {
+    primary: {
+      dark: "#a18005",
+      main: "#e7b708",
+      light: "#ebc539"
+    }
+  }
+});
+
+export const Sage = createMuiTheme({
+  palette: {
+    primary: {
+      dark: "#5a331e",
+      main: "#81492c",
+      light: "#9a6d56"
+    }
+  }
+});
