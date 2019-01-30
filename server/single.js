80b2883243b78bd1a5babe1201c0dc9343cb198e
Emmanuel Bravo <eebravo1@gmail.com>
2019-01-30 12:21:28 -0600

    added allies and opponents endpoints

--- server/single.js
+++ server/single.js
@@ -122,5 +122,80 @@ module.exports = {
 			message: `archetype of ${shadow} retrieved`,
 			types: shadowQuery
 		});
+	},
+	getOpponents: (req, res) => {
+		const name = req.params.name.toLowerCase();
+		const nameQuery = db.types.filter((e) => {
+			return e.name.includes(name) || e.alias.includes(name);
+		})[0];
+		const opponentDriveQuery = db.types.filter(
+			(e) => e.drive == nameQuery.drive
+		);
+		const opponentAspectQuery = db.types.filter(
+			(e) => e.aspect == nameQuery.aspect
+		);
+		const opponentMethodQuery = db.types.filter(
+			(e) => e.method == nameQuery.method
+		);
+		const opponentRoleQuery = db.types.filter((e) => e.role == nameQuery.role);
+		const opponents = {
+			driveOpponents: opponentDriveQuery,
+			aspectOpponents: opponentAspectQuery,
+			methodOpponents: opponentMethodQuery,
+			roleOpponents: opponentRoleQuery,
+			shadowOpponents: [nameQuery]
+		};
+
+		const result = { opponents: opponents };
+		res.status(200).send({
+			success: 'true',
+			message: `opponents of ${name} retrieved`,
+			types: result
+		});
+	},
+	getAllies: (req, res) => {
+		const name = req.params.name.toLowerCase();
+		const nameQuery = db.types.filter((e) => {
+			return e.name.includes(name) || e.alias.includes(name);
+		})[0];
+		const allyStudentQuery = db.types.filter(
+			(e) => e.role == 'student' && e.drive !== nameQuery.drive
+		);
+		const allyOutlierQuery = db.types.filter(
+			(e) => e.role == 'outlier' && e.drive !== nameQuery.drive
+		);
+		const allyMasterQuery = db.types.filter(
+			(e) => e.role == 'master' && e.drive !== nameQuery.drive
+		);
+		const allies = {
+			adjacent: {
+				studentAllies: allyStudentQuery.filter(
+					(e) => e.drive !== db.allyMatrix[nameQuery.drive]
+				),
+				outlierAllies: allyOutlierQuery.filter(
+					(e) => e.drive !== db.allyMatrix[nameQuery.drive]
+				),
+				masterAllies: allyMasterQuery.filter(
+					(e) => e.drive !== db.allyMatrix[nameQuery.drive]
+				)
+			},
+			opposing: {
+				studentAllies: allyStudentQuery.filter(
+					(e) => e.drive == db.allyMatrix[nameQuery.drive]
+				),
+				outlierAllies: allyOutlierQuery.filter(
+					(e) => e.drive == db.allyMatrix[nameQuery.drive]
+				),
+				masterAllies: allyMasterQuery.filter(
+					(e) => e.drive == db.allyMatrix[nameQuery.drive]
+				)
+			}
+		};
+		const result = { allies: allies };
+		res.status(200).send({
+			success: 'true',
+			message: `allies of ${name} retrieved`,
+			types: result
+		});
 	}
 };
